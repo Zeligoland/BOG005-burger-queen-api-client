ea119e14ee6183c03a6430784612e21b6cfb4200
Sofia Bustos <s.bustos22@hotmail.com>
2022-11-30 13:13:10 -0500

    Agrega codigo para definir el ruteado para las vistas Login, Waiter, Chef y Admin. Se carga imagen de logo d

--- burgerqueen/src/index.js
+++ burgerqueen/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './routes/Login';
-import { Roles } from './routes/Roles';
 import { Waiters } from './routes/Waiters';
 import { Chef } from './routes/Chef';
 import { Admin } from './routes/Admin';
@@ -13,8 +12,7 @@ ReactDOM.render(
 <BrowserRouter>
     <Routes>
         <Route path='/' element={<Layout/>}>       
-            <Route index element={<Login />} />
-            <Route path='Roles' element={<Roles />} />
+            <Route index element={<Login />} />            
             <Route path='waitersView' element={<Waiters />} />
             <Route path='chefView' element={<Chef/>} />
             <Route path='adminView' element={<Admin />} />
