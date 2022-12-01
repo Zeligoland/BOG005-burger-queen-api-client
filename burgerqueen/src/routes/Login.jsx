f2731b203c54d50a4a99767f0b2af855c203a0d3
Andrea Zapata <95andreazapata@gmail.com>
2022-12-01 13:35:50 -0500

    Creating Login Page

--- burgerqueen/src/routes/Login.jsx
+++ burgerqueen/src/routes/Login.jsx
@@ -1,10 +1,35 @@
-import "../index.css";
+import "../styles/login.css";
+import logoImg from "../assets/burger-queen-logo.png";
 
 export function Login() {
   return (
-    <div>
-      <h1 className="card">Ingresar</h1>
-      <h5 className="welcome">Bienvenid@ a la app de BurgerQueen</h5>
-    </div>
+    <main>
+      <div className="welcomeHeader">
+        <img src={logoImg} alt="Logoimg"/>
+        <h5 className="welcome">
+          Bienvenid@ al portal del restaurante BurgerQueen
+        </h5>
+      </div>
+
+      <section className="loginForm">
+        <div className="loginBox">
+          <h4 className="bqApp">Burger Queen App</h4>
+          <p className="authUser">Usuario BQ autorizado</p>
+          <input
+            className="userEmail"
+            type="email"
+            placeholder="Correo electrónico"
+            name="userEmail"
+          ></input>
+          <input
+            className="userPass"
+            type="password"
+            placeholder="Contraseña"
+            name="userPassword"
+          ></input>
+          <button className="signIn">Ingresar</button>
+        </div>
+      </section>
+    </main>
   );
 }
