628a7b265d9ab8b0fa6d16792916a632af32149c
Andrea Zapata <95andreazapata@gmail.com>
2022-12-06 20:24:31 -0500

    Update react-hook & axios

--- burgerqueen/src/routes/Login.jsx
+++ burgerqueen/src/routes/Login.jsx
@@ -1,11 +1,25 @@
 import "../styles/Login.css";
 import logoImg from "../assets/burger-queen-logo.png";
+import { useNavigate } from "react-router";
+import {loginUsers} from "../helpers/axios.js"
 
 export function Login() {
+  const navigate = useNavigate();
+  const login = () => {
+    loginUsers("mesero@burgerqueen.com", "123456")
+      .then(function (response) {
+        console.log(response);
+        navigate("/menuOptions");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <main>
       <div className="welcomeHeader">
-        <img src={logoImg} alt="Logoimg"/>
+        <img src={logoImg} alt="Logoimg" />
         <h5 className="welcome">
           Bienvenid@ al portal del restaurante BurgerQueen
         </h5>
@@ -27,7 +41,7 @@ export function Login() {
             placeholder="Contraseña"
             name="userPassword"
           ></input>
-          <button className="signIn">Ingresar</button>
+          <button className="signIn" onClick= {login}>Ingresar</button>
         </div>
       </section>
     </main>
